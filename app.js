a3b27221b92bca6f07040dfa956dbc79cccd7eb0
asmar.ali <asmar.ali@constell8.com>
2025-05-27 15:48:53 +0500

    updates

--- app.js
+++ app.js
@@ -1,16 +1,49 @@
-const express = require('express');
+const express = require("express");
+const {
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLList,
+} = require("graphql");
+const { createHandler } = require("graphql-http/lib/use/express");
+// const { renderGraphiQL } = require('graphiql');
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
-
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+const schema = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name: "events",
+    fields: {
+      events: {
+        type: new GraphQLList(GraphQLString),
+        resolve: () => ["testing", "coding"],
+      },
+    },
+  }),
+  mutation: new GraphQLObjectType({
+    name: "RootMutation",
+    fields: {
+      createEvent: {
+        type: GraphQLString,
+        args: {
+          name: { type: GraphQLString },
+        },
+        resolve: (parent, args) => {
+          return args.name;
+        },
+      },
+    },
+  }),
 });
 
 
+app.use("/graphql", createHandler({ 
+  schema,
+  graphiql: true 
+}));
 
-
-
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
